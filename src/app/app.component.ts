fec8ac27bd36f908e587f8c283be3e57f96eea18
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-24 19:12:12 +0800

    Master/Details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice Alice' },
+  { id: 12, name: 'Narco. Love' },
+  { id: 13, name: 'Bombasto. Story' },
+  { id: 14, name: 'Celeritas. Books' },
+  { id: 15, name: 'Magneta. Rain' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,9 +25,10 @@ export class Hero {
 })
 export class AppComponent {
   title = 'Tour of Heros';
-  // name = 'GengYuDong';
-  hero: Hero = {
-      id: 1,
-      name: 'Yudong Geng'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
