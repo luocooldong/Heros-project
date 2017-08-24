f6797f233c30f47c2b2b1f35f7404dcbb7686679
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-24 18:42:13 +0800

    Frist Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heros';
+  // name = 'GengYuDong';
+  hero: Hero = {
+      id: 1,
+      name: 'Yudong Geng'
+  };
+}
